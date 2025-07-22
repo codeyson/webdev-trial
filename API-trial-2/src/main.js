32f7258a2ff4134b13e7f013b5a0b18b12ead2b6
Fred <frederickdianzon@gmail.com>
2025-07-22 16:47:17 +0800

    trying new API

---
+++ API-trial-2/src/main.js
@@ -0,0 +1,31 @@
+import './style.css'
+
+async function generateQuotes() {
+  const input = document.querySelector('#input')
+  const button = document.querySelector('#button')
+  const output = document.querySelector('#output')
+
+  const count = 2;
+  
+  if (!input) {
+    alert('Please enter a number');
+    return;
+  }
+
+const url = `https://api.quotable.io/random`;
+
+
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+      
+  } catch (error) {
+    console.error(error);
+  }
+
+}
+
+generateQuotes();
