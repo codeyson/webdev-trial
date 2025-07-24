cda505fed90a0cde69a35f185d148150d5910d19
Fred <frederickdianzon@gmail.com>
2025-07-24 22:08:11 +0800

    modify style of passed message

--- js-trial-2/src/main.js
+++ js-trial-2/src/main.js
@@ -1,13 +1,25 @@
 import './style.css'
 
+
+
+
+
+function setMessage() {
+
 const message = document.getElementById('message');
 const input = document.querySelector('input');
 const send = document.getElementById('send');
 const passed = document.getElementById('passed');
 
-
 send.addEventListener('click', () => {
   message.content = input.value;
   input.value = '';
   passed.innerHTML = message.content;
+  passed.style.padding = "5px  4px";
+  passed.style.borderRadius = "5px"; 
+  passed.style.backgroundColor = 'red';
+
 })
+}
+
+setMessage();
