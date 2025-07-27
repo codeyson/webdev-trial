9ee3d9fec1f17f3fd625ece27248879f61d79657
Fred Masangkay <frederickdianzon@gmail.com>
2025-07-27 21:12:00 +0800

    this directory is for TOP practice

---
+++ react-trial-3/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+function Button(props) {
+  const buttonStyle = {
+    color: props.color,
+    fontSize: props.fontSize + 'px'
+  };
+
+
+  return (
+    <button style={buttonStyle}>{props.text}</button>
+  );
+}
+
+function Button2() {
+  return (
+    <button>Don't Click Me!</button>
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <Button text="Click Me!" color="blue" fontSize={12} />
+      <Button text="Don't Click Me!" color="red" fontSize={12} />
+      <Button text="Click Me!" color="blue" fontSize={20} />
+    </div>
+  );
+}
