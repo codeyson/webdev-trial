4099f5b367db2e3fb75242a494082c6833692dec
Fred <frederickdianzon@gmail.com>
2025-07-28 21:56:28 +0800

    added PERSONAL section of the CV

--- TOP-CV-Application/src/components/Personal.jsx
+++ TOP-CV-Application/src/components/Personal.jsx
@@ -4,7 +4,17 @@ import '../styles/Personal.css'
 function Personal () {
     return (
         <>
-        <div>personal</div>
+        <section className='personal'>
+            <div>
+                <img className='img-personal' src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="img-profile" />
+            </div>
+            <div className='info-personal'>
+                <h1 className='name'>Frederick Masangkay</h1>
+                <p className='email'>qwerty@gmail.com</p>
+                <p className='phone'>09123456789</p>
+            </div>
+            
+        </section>
         </>
     )
 
