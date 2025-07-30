f4c252fd6c2d60884c2b3d6c6aefc21d8a033eea
Fred <frederickdianzon@gmail.com>
2025-07-30 23:34:08 +0800

    added EDUCATION section and some styling

--- TOP-CV-Application/src/components/Education.jsx
+++ TOP-CV-Application/src/components/Education.jsx
@@ -3,7 +3,16 @@ import '../styles/Education.css'
 function Education () {
     return (
         <>
-        <div className="educ">education</div>
+        <section className='education'>
+            
+        <div className='info-education'>
+            <h1>Education</h1>
+            <p>Technological Institute of the Philippines - Quezon City</p>
+            <p>Bachelor in Computer Engineering</p>
+            <p>2023 - 2027</p>
+        </div>
+            
+        </section>
         </>
     )
 
