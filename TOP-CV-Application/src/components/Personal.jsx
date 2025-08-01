983c9aab5db6ad8102528e6eab88f6884fda7148
Fred <frederickdianzon@gmail.com>
2025-08-01 23:11:00 +0800

    change input in personal info

--- TOP-CV-Application/src/components/Personal.jsx
+++ TOP-CV-Application/src/components/Personal.jsx
@@ -9,9 +9,10 @@ function Personal () {
                 <img className='img-personal' src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="img-profile" />
             </div>
             <div className='info-personal'>
-                <h1 className='name'>Frederick Masangkay</h1>
-                <p className='email'>qwerty@gmail.com</p>
-                <p className='phone'>09123456789</p>
+                <h1 className='name'>Personal Information</h1>
+                <input type="text" placeholder='Write your name:'/>
+                <input type="text" placeholder='Write your email:'/>
+                <input type="text" placeholder='Write your phone:'/>
             </div>
             
         </section>
