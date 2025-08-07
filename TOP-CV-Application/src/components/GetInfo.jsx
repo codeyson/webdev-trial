f0a4826565e26f0024ae784052c676b4c1a9606c
Fred <frederickdianzon@gmail.com>
2025-08-07 23:49:20 +0800

    added mai nfucntion

--- TOP-CV-Application/src/components/GetInfo.jsx
+++ TOP-CV-Application/src/components/GetInfo.jsx
@@ -1,14 +1,35 @@
-function GetInfo () {
-    return (
-        <>
-        <input type="text" placeholder="Enter your name" />
-        <input type="text" placeholder="Enter your email" />
-        <input type="text" placeholder="Enter your phone" />
-        
-        </>
-    )
+import { useState } from 'react';
 
+export default function GetInfo() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Enter your email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Enter your phone"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
+
+      <h1>Personal Info Preview</h1>
+      <h2>Name: {name}</h2>
+      <h2>Email: {email}</h2>
+      <h2>Phone: {phone}</h2>
+    </div>
+  );
 }
-
-
-export default GetInfo
