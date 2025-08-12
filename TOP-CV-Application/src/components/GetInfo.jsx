f399d2996f977004b935f4ebf36380189d88a0a8
Fred <frederickdianzon@gmail.com>
2025-08-12 23:52:51 +0800

    added save to pdf fucntionaltiy

--- TOP-CV-Application/src/components/GetInfo.jsx
+++ TOP-CV-Application/src/components/GetInfo.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
+import jsPDF from 'jspdf';
 import '../styles/GetInfo.css';
 
 export default function GetInfo() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
+
+  const SavePDF = () => {
+    const doc = new jsPDF();
+    doc.text(`Name: ${name}`, 10, 10);
+    doc.text(`Email: ${email}`, 10, 20);
+    doc.text(`Phone: ${phone}`, 10, 30);
+    doc.save('info.pdf');
+  };
 
   return (
     <div>
@@ -13,26 +22,29 @@ export default function GetInfo() {
         placeholder="Enter your name"
         value={name}
         onChange={(e) => setName(e.target.value)}
-        className='input'
+        className="input"
       />
       <input
         type="text"
         placeholder="Enter your email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
-        className='input'/>
+        className="input"
+      />
       <input
         type="text"
         placeholder="Enter your phone"
         value={phone}
         onChange={(e) => setPhone(e.target.value)}
-        className='input'
+        className="input"
       />
 
       <h1>Personal Info Preview</h1>
       <h2>Name: {name}</h2>
       <h2>Email: {email}</h2>
       <h2>Phone: {phone}</h2>
+
+      <button onClick={SavePDF}>Save as pdf</button>
     </div>
   );
 }
