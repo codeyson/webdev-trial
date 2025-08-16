9bdd4ceee92f4785d0478328063e5c80fc8a328f
Fred <frederickdianzon@gmail.com>
2025-08-16 22:22:49 +0800

    class based component

---
+++ react-trial-4/src/App.jsx
@@ -0,0 +1,14 @@
+import FunctionalInput from './components/FunctionalInput.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <FunctionalInput name="First" />
+    </div>
+  )
+
+}
+
+export default App
